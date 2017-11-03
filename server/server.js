35509a0c0fea9df2b559454b9d921cce9d32ea29
Carlos Garcia <chars24@gmail.com>
2017-11-03 13:46:24 -0600

    Updating a Resources - PATCH/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash')
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -45,20 +46,54 @@ app.get('/todos/:id', (req, res) => {
     res.send({todo})
   }).catch((e) => {res.status(400).send()});
 });
-// Nueva ruta para eliminar un document por ID
+
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id
 
   if(!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  // Aplicamos el metodo para remover por ID
+
   Todo.findByIdAndRemove(id).then((todo) => {
     if(!todo) {
       return res.status(404).send();
     }
     res.send({todo})
   }).catch((e) => {res.status(404).send()});
+});
+// Ruta para actualizar documentos
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  // Esto lo hace lodash(_.), le pasamos los campos que 
+  // el usuario puede actualizar
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  // checamos con lodash si es boleano y si ya se completo
+  // la tarea y que pasa si no.
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  // $set es para setear los campos a actualizar, por eso 
+  // pasamos la varible body, la cual ya los tiene en un array
+  // new: true, es para decirle que queremos que se actualice con 
+  // los cambios.
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
 });
     
 app.listen(port, () => {
