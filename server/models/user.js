3e3db406ce64057bb77a55a58ef4d486f8ee843a
Carlos Garcia <chars24@gmail.com>
2017-11-04 23:52:36 -0600

    8. Setting Up The User Model

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,45 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    // con unique, como su nombre lo indica, 
+    // verificamos que el email sea unico.
+    unique: true,
+    // validate nos sirve para hacer una verificacion
+    // si tiene estructura de un correo
+    validate: {
+      // en validator, le pasamos la const validator
+      // declarada arriba (libreria externa)y su metodo isEmail,
+      // el cual recibe el value. Ya no es necesario hacer 
+      // una custom function.
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  // declaramos el modelo del password.
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  // tokens solo funciona para mongodb
+  // esta variable sirve para declarar los
+  // parametros que llevara los token.
+  tokens: [{
+    access:{
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -63,29 +63,23 @@ app.delete('/todos/:id', (req, res) => {
     res.send({todo})
   }).catch((e) => {res.status(404).send()});
 });
-// Ruta para actualizar documentos
+
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id
 
   if(!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  // Esto lo hace lodash(_.), le pasamos los campos que 
-  // el usuario puede actualizar
+
   var body = _.pick(req.body, ['text', 'completed']);
 
-  // checamos con lodash si es boleano y si ya se completo
-  // la tarea y que pasa si no.
   if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
     body.completedAt = null;
   }
-  // $set es para setear los campos a actualizar, por eso 
-  // pasamos la varible body, la cual ya los tiene en un array
-  // new: true, es para decirle que queremos que se actualice con 
-  // los cambios.
+
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
       return res.status(404).send();
@@ -95,9 +89,26 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
-
 });
-    
+
+// POST /users
+app.post('/users', (req, res) => {
+  // recordad _pick viene de lodash y sirve para crear
+  // un objeto con los campos que le indiquemos
+  var body = _.pick(req.body, ['email', 'password'])
+
+  // pasamos la variable body por que esta es un objeto
+  // con los valores que requerimos.
+  var user = new User(body);
+
+  user.save().then((doc) => {
+    res.send(doc);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
