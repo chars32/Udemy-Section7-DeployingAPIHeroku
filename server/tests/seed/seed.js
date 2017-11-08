a7b54214410300b1ea9940fb905b4f3ae2481219
Carlos Garcia <chars24@gmail.com>
2017-11-07 22:13:05 -0600

    8.5 Seeding Test Database With Users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,57 @@
+// Este archivo es para poder sedear la test bd y asi no
+// tener todo el codigo en server.test.js
+const { ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
+
+// Sedeamos los todos
+const todos = [{
+  _id: new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id: new ObjectID(),
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
+}];
+// limpiamos bd Todo e insertamos.
+const populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+};
+
+// declaramos id´s para los users
+const userOneId = new ObjectID();
+const userTwoId = new ObjectID();
+// Sedeamos los users
+const users = [{
+  _id: userOneId,
+  email: 'andrew@example.com',
+  password: 'userOnePass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({ _id: userOneId, access: 'auth' }, 'abc123').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'jen@example.com',
+  password: 'userTwoPass'
+}];
+// limpiamos bd Users e insertamos
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save();
+    var userTwo = new User(users[1]).save();
+    // promise.all es un nuevo concepto el cual al 
+    // igual que las promesa normal, no retorna valor
+    // hasta que se cumplan los parametros pasados.
+    return Promise.all([userOne, userTwo])
+  }).then(() => done());
+};
+
+
+
+module.exports = { todos, populateTodos, users, populateUsers }
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,20 +4,12 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo'
-}];
-
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+// Aqui es donde sedeamos las bd antes de 
+// empezar los testeos
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
