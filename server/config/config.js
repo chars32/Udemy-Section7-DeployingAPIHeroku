12f7e45bd715546f57cb791218efe74939eb2bac
Carlos Garcia <chars24@gmail.com>
2017-11-09 23:10:04 -0600

    8.13 Improving App Configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,18 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env ***', env);
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  // Cuando quieres acceder a la propiedad de un
+  // objeto se utilizan los [], aqui queremos 
+  // saber que tipo de env esta utilizando
+  var envConfig = config[env];
+
+  // Object.keys nos devuelve las keys de un objeto
+  // y luego recorremos con un forEach
+  Object.keys(envConfig).forEach((key) => {
+    // Le pasamos al process.env la llave como key
+    // y el valor de cada una de las key
+    process.env[key] = envConfig[key]
+  });
 }
